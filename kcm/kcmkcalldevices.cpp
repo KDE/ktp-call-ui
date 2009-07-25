bd7bb794910ef7bdeb6c1b31d6105a098ae7bcab
George Kiagiadakis <kiagiadakis.george@gmail.com>
2009-07-25 10:52:22 +0000

    Add a tab for configuring video output drivers in kcm_kcalldevices.
    svn path=/trunk/playground/network/kcall/; revision=1002198

--- kcm/kcmkcalldevices.cpp
+++ kcm/kcmkcalldevices.cpp
@@ -36,6 +36,7 @@ struct KcmKcallDevices::Private
     TableDeviceChooser *audioInputChooser;
     TableDeviceChooser *audioOutputChooser;
     TableDeviceChooser *videoInputChooser;
+    TableDeviceChooser *videoOutputChooser;
 };
 
 KcmKcallDevices::KcmKcallDevices(QWidget *parent, const QVariantList & args)
@@ -53,9 +54,11 @@ KcmKcallDevices::KcmKcallDevices(QWidget *parent, const QVariantList & args)
     d->audioInputChooser = new TableDeviceChooser(d->manager, DeviceManager::AudioInput);
     d->audioOutputChooser = new TableDeviceChooser(d->manager, DeviceManager::AudioOutput);
     d->videoInputChooser = new TableDeviceChooser(d->manager, DeviceManager::VideoInput);
+    d->videoOutputChooser = new TableDeviceChooser(d->manager, DeviceManager::VideoOutput);
     tabWidget->addTab(d->audioInputChooser, KIcon("audio-input-microphone"), i18n("Audio input device"));
     tabWidget->addTab(d->audioOutputChooser, KIcon("audio-card"), i18n("Audio output device"));
     tabWidget->addTab(d->videoInputChooser, KIcon("camera-web"), i18n("Video input device"));
+    tabWidget->addTab(d->videoOutputChooser, KIcon("video-display"), i18n("Video output driver"));
 }
 
 KcmKcallDevices::~KcmKcallDevices()

63a01e61a1ddba295d50d42583cc60d2a0b91b83
George Kiagiadakis <kiagiadakis.george@gmail.com>
2009-08-13 14:53:02 +0000

    Add basic documentation for QTfChannel.
    svn path=/trunk/playground/network/kcall/; revision=1010866

--- libqtpfarsight/qtfchannel.h
+++ libqtpfarsight/qtfchannel.h
@@ -21,35 +21,96 @@
 #include "../libqtgstreamer/qgstelement.h"
 #include <TelepathyQt4/StreamedMediaChannel>
 
-/** This is a wrapper class around TfChannel and TfStream that provides a saner Qt-ish API */
+/** This is a wrapper class around TfChannel and TfStream that provides a saner Qt-ish API.
+ * The API is highly asynchronous. Everything is done through signals. The caller needs to connect
+ * to all these signals and react appropriately on them.
+ */
 class QTfChannel : public QObject
 {
     Q_OBJECT
 public:
-    QTfChannel(Tp::StreamedMediaChannelPtr channel, QtGstreamer::QGstBusPtr bus, QObject *parent = 0);
+    /** Constructs a QTfChannel that corresponds to the specified @a channel.
+     * @a bus should be the GstBus of the pipeline.
+     */
+    QTfChannel(Tp::StreamedMediaChannelPtr channel, QtGstreamer::QGstBusPtr bus,
+               QObject *parent = 0);
     virtual ~QTfChannel();
 
+    /** Sets the filename of the file where codec preferences should be read from.
+     * The filename is passed to fs_codec_list_from_keyfile(). See the documentation of
+     * that method (in the Farsight2 docs) for more information on the file format.
+     */
     void setCodecsConfigFile(const QString & filename);
-    void setGstBus(QtGstreamer::QGstBusPtr bus);
 
 Q_SIGNALS:
+    /** Emmited when the session starts. @a conference is the FsConference element
+     * that should be added in the pipeline and set to PLAYING state.
+     */
     void sessionCreated(QtGstreamer::QGstElementPtr conference);
+
+    /** Emmited when the channel is closed. The pipeline should be stopped when
+     * this signal is received.
+     */
     void closed();
 
+
+    /** Emmited when the audio input device should be initialized. @a suceess should
+     * be set to true from the slot if the device was initialized properly and can be used.
+     */
     void openAudioInputDevice(bool *success);
+
+    /** Emmited when an audio sink pad is added on the conference.
+     * @a sinkPad should be connected with the audio input device.
+     */
     void audioSinkPadAdded(QtGstreamer::QGstPadPtr sinkPad);
+
+    /** Emmited when the audio input device can be closed */
     void closeAudioInputDevice();
 
+
+    /** Emmited when the video input device should be initialized. @a suceess should
+     * be set to true from the slot if the device was initialized properly and can be used.
+     */
     void openVideoInputDevice(bool *success);
+
+    /** Emmited when a video sink pad is added on the conference.
+     * @a sinkPad should be connected with the video input device.
+     */
     void videoSinkPadAdded(QtGstreamer::QGstPadPtr sinkPad);
+
+    /** Emmited when the video input device can be closed */
     void closeVideoInputDevice();
 
+
+    /** Emmited when the audio output device should be initialized. @a suceess should
+     * be set to true from the slot if the device was initialized properly and can be used.
+     */
     void openAudioOutputDevice(bool *success);
+
+    /** Emmited when an audio source pad is added on the conference.
+     * @a srcPad should be connected with the audio output device.
+     */
     void audioSrcPadAdded(QtGstreamer::QGstPadPtr srcPad);
+
+    /** Emmited when an audio source pad is removed from the conference.
+     * @a srcPad should be disconnected from the audio output device.
+     */
     void audioSrcPadRemoved(QtGstreamer::QGstPadPtr srcPad);
 
+
+    /** Emmited when the video output device should be initialized. @a suceess should
+     * be set to true from the slot if the device was initialized properly and can be used.
+     */
     void openVideoOutputDevice(bool *success);
+
+    /** Emmited when a video source pad is added on the conference.
+     * @a srcPad should be connected with a video widget.
+     */
     void videoSrcPadAdded(QtGstreamer::QGstPadPtr srcPad);
+
+    /** Emmited when a video source pad is removed from the conference.
+     * @a srcPad should be disconnected from the video widget.
+     */
     void videoSrcPadRemoved(QtGstreamer::QGstPadPtr srcPad);
 
 private:

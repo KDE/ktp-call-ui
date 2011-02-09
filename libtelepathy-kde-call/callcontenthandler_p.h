74033b70297b2a7da0581039cafd1f5e26dcf128
George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
2011-02-09 14:02:58 +0200

    Refactor libtelepathy-kde-call.
    The new code is meant to work with the new Call spec and telepathy-farstream.
    I think the design is much cleaner now, since the handling has been split
    into many different classes that do a small part each. Also, the code
    should be much more stable now, as the sources and sinks have been refactored
    to take care of possible race conditions with the gstreamer streaming threads.

---
+++ libtelepathy-kde-call/callcontenthandler_p.h
@@ -0,0 +1,95 @@
+/*
+    Copyright (C) 2011 Collabora Ltd. <info@collabora.co.uk>
+      @author George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CALLCONTENTHANDLER_P_H
+#define CALLCONTENTHANDLER_P_H
+
+#include "callcontenthandler.h"
+#include "../libqtf/qtf.h"
+#include <QGst/Pipeline>
+#include <QGst/Pad>
+
+class BaseSinkManager;
+
+/* This class constructs a CallContentHandler. This operation is asynchronous
+ * because we need to construct a CallContentHandler as soon as we receive
+ * the TfContent from tp-farstream, however, because tp-qt4 and tp-glib check
+ * dbus separately, tp-qt4 may not be synchronized with tp-glib and thus the
+ * Tp::CallContent may not be available when the TfContent comes. This class
+ * basically waits for tp-qt4 to synchronize.
+ */
+class PendingCallContentHandler : public QObject
+{
+    Q_OBJECT
+public:
+    PendingCallContentHandler(const Tpy::CallChannelPtr & callChannel,
+                              const QTf::ContentPtr & tfContent,
+                              const QGst::PipelinePtr & pipeline,
+                              QObject *parent);
+
+private Q_SLOTS:
+    void findCallContent();
+    void onContentAdded(const Tpy::CallContentPtr & callContent);
+
+Q_SIGNALS:
+    void ready(const QTf::ContentPtr & tfContent, CallContentHandler *contentHandler);
+
+private:
+    CallContentHandler *m_contentHandler;
+    Tpy::CallChannelPtr m_callChannel;
+    QTf::ContentPtr m_tfContent;
+};
+
+
+class CallContentHandlerPrivate : public QObject
+{
+    Q_OBJECT
+public:
+    CallContentHandlerPrivate(CallContentHandler *qq) : QObject(), q(qq) {}
+    virtual ~CallContentHandlerPrivate();
+
+    /* Called from PendingCallContentHandler right after constructing the CallContentHandler */
+    void init(const QTf::ContentPtr & tfContent, const QGst::PipelinePtr & pipeline);
+
+    /* Called from PendingCallContentHandler when the CallContent is ready */
+    void setCallContent(const Tpy::CallContentPtr & callContent);
+
+private:
+    void onSrcPadAdded(uint contactHandle,
+                       const QGlib::ObjectPtr & fsStream,
+                       const QGst::PadPtr & pad);
+    bool onStartSending();
+    bool onStopSending();
+
+private Q_SLOTS:
+    void onControllerCreated(BaseSinkController *controller);
+    void onControllerDestroyed(BaseSinkController *controller);
+
+public: //accessed from the public class
+    BaseSourceController *m_sourceController;
+    QSet<BaseSinkController*> m_sinkControllers;
+    Tpy::CallContentPtr m_callContent;
+
+private:
+    CallContentHandler * const q;
+    BaseSinkManager *m_sinkManager;
+    QTf::ContentPtr m_tfContent;
+    QGst::PipelinePtr m_pipeline;
+    QGst::PadPtr m_sourceControllerPad;
+};
+
+#endif // CALLCONTENTHANDLER_P_H
---
+++ libtelepathy-kde-call/tests/sourcetest.cpp
@@ -0,0 +1,87 @@
+/*
+    Copyright (C) 2011 Collabora Ltd. <info@collabora.co.uk>
+      @author George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "../sourcecontrollers.h"
+#include <QGst/Init>
+#include <QGst/Pipeline>
+#include <QGst/Pad>
+#include <QGst/ElementFactory>
+#include <QtCore/QCoreApplication>
+#include <KDebug>
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    QGst::init(&argc, &argv);
+
+    QGst::PipelinePtr pipeline = QGst::Pipeline::create();
+
+    QGst::ElementPtr sink = QGst::ElementFactory::make("autoaudiosink");
+    sink->setState(QGst::StateReady);
+
+    pipeline->add(sink);
+    pipeline->setState(QGst::StatePlaying);
+
+    QGst::PadPtr sinkPad = sink->getStaticPad("sink");
+    QElapsedTimer timer;
+    AudioSourceController *sc = new AudioSourceController(pipeline);
+
+    kDebug() << "adding...";
+    timer.start();
+    QGst::PadPtr srcPad = sc->requestSrcPad();
+    srcPad->link(sinkPad);
+    kDebug() << "added" << timer.elapsed();
+
+    sleep(3);
+
+    kDebug() << "connecting...";
+    timer.restart();
+    sc->setSourceEnabled(true);
+    kDebug() << "connected" << timer.elapsed();
+
+    sleep(3);
+
+    kDebug() << "disconnecting...";
+    timer.restart();
+    sc->setSourceEnabled(false);
+    kDebug() << "disconnected" << timer.elapsed();
+
+    sleep(3);
+
+    kDebug() << "connecting...";
+    timer.restart();
+    sc->setSourceEnabled(true);
+    kDebug() << "connected" << timer.elapsed();
+
+    sleep(3);
+
+    kDebug() << "disconnecting...";
+    timer.restart();
+    sc->setSourceEnabled(false);
+    kDebug() << "disconnected" << timer.elapsed();
+
+    kDebug() << "removing...";
+    timer.restart();
+    srcPad->unlink(sinkPad);
+    sc->releaseSrcPad(srcPad);
+    kDebug() << "removed" << timer.elapsed();
+
+    delete sc;
+    pipeline->setState(QGst::StateNull);
+
+    return 0;
+}

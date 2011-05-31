52458508fadcbeba2cd784893df67d439d3f288d
Pino Toscano <pino@kde.org>
2011-05-31 15:57:55 +0200

    i18n: rename the extracted catalog from kcall.pot to telepathy-kde-call-ui.pot
    acked by George Kiagiadakis

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kcall.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/telepathy-kde-call-ui.pot
 rm -f rc.cpp

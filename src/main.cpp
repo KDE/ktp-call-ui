c476546d4746300a01619db899fc11c4e2825872
David Edmundson <kde@davidedmundson.co.uk>
2013-02-16 13:12:03 +0000

    Bump version to 0.5.3

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 
 int main(int argc, char **argv)
 {
-    KAboutData aboutData("ktp-call-ui", 0, ki18n("KDE Telepathy Call Ui"), "0.5.2",
+    KAboutData aboutData("ktp-call-ui", 0, ki18n("KDE Telepathy Call Ui"), "0.5.3",
                           ki18n("VoIP client for KDE"), KAboutData::License_GPL,
                           ki18n("(C) 2009-2012, George Kiagiadakis\n"
                                 "(C) 2010-2011, Collabora Ltd."));
